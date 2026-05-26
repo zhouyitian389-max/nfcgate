02309efdcf40690305d462fd2675618a4297cc30
zhouyitian389-max <zhouyitian389@gmail.com>
2026-05-26 16:24:48 +0800

    feat: add server/src/services/sseHub.ts

---
+++ server/src/services/sseHub.ts
@@ -0,0 +1,38 @@
+import { ServerResponse } from 'http';
+import { randomUUID } from 'crypto';
+
+interface SSEClient {
+  id: string;
+  userId: string;
+  res: ServerResponse;
+}
+
+class SSEHub {
+  private clients: Map<string, SSEClient> = new Map();
+
+  addClient(userId: string, res: ServerResponse): string {
+    const id = randomUUID();
+    this.clients.set(id, { id, userId, res });
+    return id;
+  }
+
+  removeClient(id: string) {
+    this.clients.delete(id);
+  }
+
+  sendToUser(userId: string, event: string, data: any) {
+    for (const client of this.clients.values()) {
+      if (client.userId === userId) {
+        client.res.write(`event: ${event}\ndata: ${JSON.stringify(data)}\n\n`);
+      }
+    }
+  }
+
+  broadcast(event: string, data: any) {
+    for (const client of this.clients.values()) {
+      client.res.write(`event: ${event}\ndata: ${JSON.stringify(data)}\n\n`);
+    }
+  }
+}
+
+export const sseHub = new SSEHub();
